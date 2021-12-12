a56f321ecee0a2ee9f39b81e16a6b9270df8ddd9
Saveliy <ssandry.github@gmail.com>
2021-12-12 15:26:44 +0300

    Add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 import { Telegraf } from "telegraf";
 
-const bot = new Telegraf("5062502033:AAHlmVKihcf5gbp33pqinpmqf_F7tMT6038");
+const bot = new Telegraf(process.env.TOKEN);
 
 bot.start((ctx) => {
   ctx.reply("Welcome to tmusicfy bot. For search just enter your query");
