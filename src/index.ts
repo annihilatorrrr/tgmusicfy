a3fc2d681a6763690ddd2b6f6d21472cf32994ed
Saveliy <ssandry.github@gmail.com>
2021-12-12 15:23:28 +0300

    Initial project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("fdfd");
+import { Telegraf } from "telegraf";
+
+const bot = new Telegraf("5062502033:AAHlmVKihcf5gbp33pqinpmqf_F7tMT6038");
+
+bot.start((ctx) => {
+  ctx.reply("Welcome to tmusicfy bot. For search just enter your query");
+  ctx.reply("Author: @ssandry");
+  ctx.reply("Made with love!");
+});
+
+bot.launch();
