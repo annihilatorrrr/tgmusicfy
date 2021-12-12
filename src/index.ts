b0926bf7107883e89dc7a3ee27bab2c021c6831c
Saveliy <ssandry.github@gmail.com>
2021-12-12 16:54:25 +0300

    Initial workspace

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 import { Telegraf } from "telegraf";
+import axios, { AxiosResponse } from "axios";
+import cheerio from "cheerio";
 
 const bot = new Telegraf(process.env.TOKEN);
 
@@ -8,6 +10,15 @@ bot.start((ctx) => {
   ctx.reply("Welcome to tmusicfy bot. For search just enter your query");
   ctx.reply("Author: @ssandry");
   ctx.reply("Made with love!");
+
+  const $ = cheerio.load('<h2 class="title">Hello world</h2>');
+});
+
+bot.on("text", async (ctx) => {
+  const queryString = ctx.message.text.replaceAll(" ", "%20");
+
+  const { data }: AxiosResponse<string> = await axios.get(`https://my.mp3ha.org/search/${queryString}`);
+  const $ = cheerio.load(data);
 });
 
 bot.launch();
