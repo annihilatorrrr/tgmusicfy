e0f7d0c6a64d849a49b7c58b5eab47f7d41fbf0f
Saveliy Andronov <ssandry.github@gmail.com>
2021-12-17 05:06:09 +0300

    Update isMorning.ts

--- src/utils/isMorning.ts
+++ src/utils/isMorning.ts
@@ -1,6 +1,6 @@
 export default function isMorning(ctx: any): boolean {
   if (ctx.state.lastMessage) {
-    if (ctx.state.lastMessage.getDate() !== new Date().getDate() && new Date().getHours() < 9 && new Date().getHours() < 4) {
+    if (ctx.state.lastMessage.getDate() !== new Date().getDate() && new Date().getHours() < 9 && new Date().getHours() > 4) {
       return true;
     }
     return false;
