c8972668de3287a93f07b0e6858534d7a88025cd
Saveliy Andronov <ssandry.github@gmail.com>
2021-12-21 22:56:17 +0300

    Fix main bot code

--- src/bot.ts
+++ src/bot.ts
@@ -1,46 +1,27 @@
 import { Context, Telegraf } from "telegraf";
 import { Update } from "telegraf/typings/core/types/typegram";
 import cheerio, { CheerioAPI } from "cheerio";
-import createResults from "./utils/createResults";
 import getData from "./utils/getData";
-import isMorning from "./utils/isMorning";
-import { getRandomHeart, getRandomGoodMorningText, getRandomNoResultsText, getRandomGoodMorningEmoji } from "./utils/randomText";
+import { getRandomHeart } from "./utils/randomText";
+import sendResults from "./utils/actions/sendResults";
 
 export default function startBot(bot: Telegraf<Context<Update>>) {
   bot.start(async (ctx) => {
     await ctx.reply(`Welcome, ${ctx.message.from.first_name}. \n \n To search, simply enter the name of the artist, song`);
+    await ctx.reply("See /help for a list of commands");
     await ctx.reply("Author: @ssandry0");
     await ctx.reply(getRandomHeart());
   });
 
   bot.on("text", async (ctx, next) => {
     if (ctx.message.text[0] !== "/") {
-      const isCurrentTimeMorning: boolean = isMorning();
-
       try {
         await ctx.reply("🔎");
 
         const data: string = await getData(ctx.message.text);
         const $: CheerioAPI = cheerio.load(data);
 
-        if ($(".list-view .audio").toArray().length > 1) {
-          const promises = createResults($).map(async (result) => {
-            try {
-              return await ctx.replyWithAudio({ url: result.audio }, { title: result.title, performer: result.performer });
-            } catch (error) {
-              ctx.reply("Something went wrong when downloading the file. 🥺");
-            }
-          });
-          Promise.all(promises).then(() => {
-            ctx.reply(
-              `${isCurrentTimeMorning ? `${getRandomGoodMorningText()} ` : ""}Enjoy listening! ${
-                isCurrentTimeMorning ? getRandomHeart() : getRandomGoodMorningEmoji()
-              }`,
-            );
-          });
-        } else {
-          await ctx.reply(`${getRandomNoResultsText()} 🥺`);
-        }
+        await sendResults($, ctx);
       } catch (error) {
         await ctx.reply("Something has gone wrong. 🥺");
       }
---
+++ src/utils/actions/sendResults.ts
@@ -0,0 +1,27 @@
+import { CheerioAPI } from "cheerio";
+import createResults from "../createResults";
+import isMorning from "../isMorning";
+import { getRandomGoodMorningEmoji, getRandomGoodMorningText, getRandomHeart, getRandomNoResultsText } from "../randomText";
+
+export default async function sendResults($: CheerioAPI, ctx: any) {
+  const isCurrentTimeMorning: boolean = isMorning();
+
+  if ($(".list-view .audio").toArray().length > 1) {
+    const promises = createResults($).map(async (result) => {
+      try {
+        return await ctx.replyWithAudio({ url: result.audio }, { title: result.title, performer: result.performer });
+      } catch (error) {
+        ctx.reply("Something went wrong when downloading the file. 🥺");
+      }
+    });
+    Promise.all(promises).then(() => {
+      ctx.reply(
+        `${isCurrentTimeMorning ? `${getRandomGoodMorningText()} ` : ""}Enjoy listening! ${
+          isCurrentTimeMorning ? getRandomHeart() : getRandomGoodMorningEmoji()
+        }`,
+      );
+    });
+  } else {
+    await ctx.reply(`${getRandomNoResultsText()} 🥺`);
+  }
+}
