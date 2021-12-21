40499d71ddad1b5db31ffdab3c96d14c67cc3586
Saveliy Andronov <ssandry.github@gmail.com>
2021-12-21 23:24:04 +0300

    Contants array

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,3 +11,15 @@ export const goodMorningTexts: string[] = [
 export const noResults: string[] = ["Nothing came up for your query.", "Unfortunately, I couldn't find anything.", "I didn't find anything."];
 export const heartsEmojies: string[] = ["❤️", "🧡", "💛", "💚", "💜", "🤍", "🤎", "💖"];
 export const goodMorningEmojies: string[] = [...heartsEmojies, "🌞", "✨", "🌟", "⭐️", "💫", "🌸", "🌺", "🌹", "🌷", "🌼", "🪴", "🍃", "🕊"];
+export const recomendations: string[] = [
+  "Chiiild",
+  "Itoko Toma",
+  "If you have no friends",
+  "The 1975",
+  "Hello Saferide",
+  "Clairo",
+  "polnalyubvi",
+  "Louis the Child",
+  "Frank Ocean",
+  "Daft Punk",
+];
