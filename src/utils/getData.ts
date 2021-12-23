5c7b0bedb6a9eb4c992b0cf4c48306607c8701ba
Saveliy Andronov <ssandry.github@gmail.com>
2021-12-23 21:42:29 +0300

    Update

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -4,11 +4,13 @@ import { isRussianLang, rusTranslit } from "./rus";
 export default async function getData(query: string): Promise<string> {
   let queryString: string;
 
-  if (isRussianLang(query)) {
-    queryString = rusTranslit(query).replaceAll(" ", "+");
-  } else {
-    queryString = query.replaceAll(" ", "+");
-  }
+  // if (isRussianLang(query)) {
+  //   queryString = rusTranslit(query).replaceAll(" ", "+");
+  // } else {
+  //   queryString = query.replaceAll(" ", "+");
+  // }
+
+  queryString = query.replaceAll(" ", "+");
 
   const { data }: AxiosResponse<string> = await axios.get(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`);
 
