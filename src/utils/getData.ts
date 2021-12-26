9ec17eaa50f337072498780a2020a4fa043f5d6c
Saveliy Andronov <ssandry.github@gmail.com>
2021-12-26 15:03:37 +0300

    Remove translit

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -4,11 +4,7 @@ import { isRussianLang, rusTranslit } from "./rus";
 export default async function getData(query: string): Promise<string> {
   let queryString: string;
 
-  if (isRussianLang(query)) {
-    queryString = rusTranslit(query).replaceAll(" ", "+");
-  } else {
-    queryString = query.replaceAll(" ", "+");
-  }
+  queryString = query.replaceAll(" ", "+");
 
   const { data }: AxiosResponse<string> = await axios.get(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`);
 
