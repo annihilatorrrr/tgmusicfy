2ec11766b6ac01b82afee5cd04014ce538ec0d12
Saveliy Andronov <ssandry.github@gmail.com>
2021-12-26 15:15:27 +0300

    Fix imports

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,12 +1,7 @@
 import axios, { AxiosResponse } from "axios";
-import { isRussianLang, rusTranslit } from "./rus";
 
 export default async function getData(query: string): Promise<string> {
-  let queryString: string;
-
-  queryString = query.replaceAll(" ", "+");
-
+  let queryString: string = query.replaceAll(" ", "+");
   const { data }: AxiosResponse<string> = await axios.get(encodeURI(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`));
-
   return data;
 }
