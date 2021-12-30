b32c42ed1dd029cc76ceb6fc1658e32d054b2678
Saveliy Andronov <ssandry.github@gmail.com>
2021-12-30 20:41:38 +0300

    Fix types

--- src/utils/createResults.ts
+++ src/utils/createResults.ts
@@ -1,9 +1,9 @@
 import { Element, CheerioAPI } from "cheerio";
 
 export default function createResults($: CheerioAPI) {
-  const audios = $(".list-view .audio").toArray().slice(0, 5);
-  const performers = $(".audio .audio-artist a").toArray().slice(0, 5);
-  const titles = $(".audio .col-lg-9").toArray().slice(0, 5);
+  const audios: Element[] = $(".list-view .audio").toArray().slice(0, 5);
+  const performers: Element[] = $(".audio .audio-artist a").toArray().slice(0, 5);
+  const titles: Element[] = $(".audio .col-lg-9").toArray().slice(0, 5);
 
   const results = audios.map((href: Element, index: number) => {
     const audio: string = href.attribs["data-url"];
