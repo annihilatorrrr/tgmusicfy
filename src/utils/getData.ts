4570636111bbbafa8c9ac565b098e5997a385cb3
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-04 14:40:41 +0300

    Refactoring code

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,7 +1,15 @@
 import axios, { AxiosResponse } from "axios";
 
-export default async function getData(query: string): Promise<string> {
-  let queryString: string = query.replaceAll(" ", "+");
-  const { data }: AxiosResponse<string> = await axios.get(encodeURI(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`));
-  return data;
+type IQuery = string | "__popular";
+
+export default async function getData(query: IQuery): Promise<string> {
+  switch (query) {
+    case "__popular":
+      const res: AxiosResponse<string> = await axios.get(encodeURI(`${process.env.MUSIC_SOURCE}/popular?only_eng=1&genreId=0`));
+      return res.data;
+    default:
+      let queryString: string = query.replaceAll(" ", "+");
+      const { data }: AxiosResponse<string> = await axios.get(encodeURI(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`));
+      return data;
+  }
 }
