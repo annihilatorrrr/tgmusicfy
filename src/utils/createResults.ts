17a857c96de06f51e98a8471ccc039c6944b48c4
Saveliy <ssandry.github@gmail.com>
2022-03-16 17:35:44 +0300

    Some refactoring

--- src/utils/createResults.ts
+++ src/utils/createResults.ts
@@ -10,12 +10,10 @@ export default function createResults($: CheerioAPI): IResult[] {
   const audios: Element[] = $(".list-view .audio").toArray().slice(0, 5);
   const performers: Element[] = $(".audio .audio-artist a").toArray().slice(0, 5);
   const titles: Element[] = $(".audio .col-lg-9").toArray().slice(0, 5);
-
   const results: IResult[] = audios.map((href: Element, index: number) => {
     const audio: string = href.attribs["data-url"];
     const performer: any = performers[index].children[0];
     const title: any = titles[index].children[0].parent.children[4];
-
     return {
       audio: audio,
       performer: performer.data,
