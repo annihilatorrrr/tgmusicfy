66df7108f1926cb536e13e4be147cd090cc72ac4
Saveliy <ssandry.github@gmail.com>
2022-03-18 20:31:41 +0300

    Add more tests requests

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -3,12 +3,18 @@ import axios, { AxiosResponse } from "axios";
 type IQuery = string | "__popular";
 
 export default async function getData(query: IQuery): Promise<{ data: string }> {
+  let testCounter = 0;
+  const testLimit = 10;
+  async function testQuery(query: string) {
+    if (testCounter <= testLimit) {
+      await axios.get(encodeURI(query));
+      testCounter = testCounter + 1;
+      testQuery(query);
+    }
+  }
   switch (query) {
     case "__popular":
-      await axios.get(
-        encodeURI(`${process.env.MUSIC_SOURCE}/popular?only_eng=1&genreId=0`),
-      );
-      await axios.get(
+      await testQuery(
         encodeURI(`${process.env.MUSIC_SOURCE}/popular?only_eng=1&genreId=0`),
       );
       const res: AxiosResponse = await axios.get(
@@ -19,8 +25,7 @@ export default async function getData(query: IQuery): Promise<{ data: string }> 
       };
     default:
       let queryString: string = query.replaceAll(" ", "+");
-      await axios.get(encodeURI(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`));
-      await axios.get(encodeURI(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`));
+      await testQuery(encodeURI(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`));
       const { data }: AxiosResponse = await axios.get(
         encodeURI(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`),
       );
