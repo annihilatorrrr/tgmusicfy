663f6bbb150794565d66a39b217eced84c4f5a4c
Saveliy <ssandry.github@gmail.com>
2022-03-18 18:35:42 +0300

    Refactoring input file

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
-require("dotenv").config();
-
 import { Context, Telegraf } from "telegraf";
 import { Update } from "telegraf/typings/core/types/typegram";
 import express, { Request, Response, Express } from "express";
 import startBot from "./bot";
 
+require("dotenv").config();
+
 const API_TOKEN: string = process.env.API_TOKEN || "";
 const PORT: string | 3000 = process.env.PORT || 3000;
 const URL: string = process.env.URL || "https://tgmusicfy.herokuapp.com/";
-
 const bot: Telegraf<Context<Update>> = new Telegraf(process.env.TOKEN);
 const expressApp: Express = express();
 
 bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
-
 expressApp.use(bot.webhookCallback(`/bot${API_TOKEN}`));
 expressApp.get("/", (req: Request, res: Response) => {
   res.send("TGMusicfy Bot");
