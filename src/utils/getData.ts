9aa4022a587010fa8d9e769d9e5caf72825022a9
Saveliy <ssandry.github@gmail.com>
2022-03-18 18:11:25 +0300

    Add pre get data. Some refactoring types

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -5,13 +5,15 @@ type IQuery = string | "__popular";
 export default async function getData(query: IQuery): Promise<{ data: string }> {
   switch (query) {
     case "__popular":
-      const res: AxiosResponse<string> = await axios.get(encodeURI(`${process.env.MUSIC_SOURCE}/popular?only_eng=1&genreId=0`));
+      await axios.get(encodeURI(`${process.env.MUSIC_SOURCE}/popular?only_eng=1&genreId=0`));
+      const res: AxiosResponse = await axios.get(encodeURI(`${process.env.MUSIC_SOURCE}/popular?only_eng=1&genreId=0`));
       return {
         data: res.data,
       };
     default:
       let queryString: string = query.replaceAll(" ", "+");
-      const { data }: AxiosResponse<string> = await axios.get(encodeURI(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`));
+      await axios.get(encodeURI(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`));
+      const { data }: AxiosResponse = await axios.get(encodeURI(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`));
       return {
         data,
       };
