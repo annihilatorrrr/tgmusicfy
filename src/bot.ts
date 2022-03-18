dd292494aebfe069f38a83debbca494a70505e39
Saveliy <ssandry.github@gmail.com>
2022-03-18 18:33:12 +0300

    Refactoring imports

--- src/bot.ts
+++ src/bot.ts
@@ -1,24 +1,24 @@
 import { Context, Telegraf } from "telegraf";
 import { Update } from "telegraf/typings/core/types/typegram";
-import { startBotCommand } from "./commands/start";
-import { helpBotCommand } from "./commands/help";
-import { textBotCommand } from "./commands/text";
-import { recomendationsBotCommand } from "./commands/recomendations";
-import { topBotCommand } from "./commands/top";
-import { analogsBotCommand } from "./commands/analogs";
-import { sourcesBotCommand } from "./commands/sources";
-import { devBotCommand } from "./commands/dev";
+import { startBotCommand as startCommand } from "./commands/start";
+import { helpBotCommand as helpCommand } from "./commands/help";
+import { textBotCommand as textHears } from "./commands/text";
+import { recomendationsBotCommand as recomendationsCommand } from "./commands/recomendations";
+import { topBotCommand as topCommand } from "./commands/top";
+import { analogsBotCommand as analogsCommand } from "./commands/analogs";
+import { sourcesBotCommand as sourcesCommand } from "./commands/sources";
+import { devBotCommand as devCommand } from "./commands/dev";
 
 export default function startBot(bot: Telegraf<Context<Update>>): void {
   [
-    startBotCommand,
-    helpBotCommand,
-    textBotCommand,
-    recomendationsBotCommand,
-    topBotCommand,
-    analogsBotCommand,
-    sourcesBotCommand,
-    devBotCommand,
+    startCommand,
+    helpCommand,
+    textHears,
+    recomendationsCommand,
+    topCommand,
+    analogsCommand,
+    sourcesCommand,
+    devCommand,
   ].map((command) => {
     command(bot);
   });
