39226655ed82f2bd48ec4be279a33a9d14142eab
Saveliy <ssandry.github@gmail.com>
2022-03-21 15:56:35 +0300

    Not a task. Adaptive response

--- src/utils/createResults.ts
+++ src/utils/createResults.ts
@@ -12,7 +12,9 @@ export default function createResults($: CheerioAPI): IResult[] {
   const performers: Element[] = $(".audio .audio-artist a").toArray().slice(0, 5);
   const titles: Element[] = $(".audio .col-lg-9").toArray().slice(0, 5);
   const results: IResult[] = audios.map((link: Element, index: number) => {
-    const audio: string = process.env.MUSIC_SOURCE + link.attribs["href"];
+    const audio: string =
+      process.env.MUSIC_SOURCE +
+      link.attribs["href"].replace("predownload?", "play?").replace("audio/", "");
     const performer: any = performers[index].children[0];
     const title: any = titles[index].children[0].parent.children[4];
     return {
