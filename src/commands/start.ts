7feaf05ddb37a25fc2dd07e443d23fdb81b2d712
Saveliy Andronov <ssandry.github@gmail.com>
2022-09-03 20:31:33 +0300

    Update start.ts

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -8,7 +8,7 @@ function startBotCommand(bot: Telegraf<Context<Update>>) {
       `Welcome, ${ctx.message.from.first_name}. \n \n To search, simply enter the name of the artist, song`,
     );
     await ctx.reply("See /help for a list of commands");
-    await ctx.reply("Author: @ssandry0");
+    await ctx.reply("Author: @hschhhwwwo0o");
     await ctx.reply(getRandomHeart());
   });
 }
