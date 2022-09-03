ff415541026471806219ce1423523dbfbdb5337c
saveliy <ssandry.github@gmail.com>
2022-09-03 22:43:03 +0300

    New build

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -4,20 +4,8 @@ import { IResponse } from "../types";
 type IQuery = string | "__popular";
 
 export default async function getData(query: IQuery): Promise<IResponse> {
-  let testCounter: number = 0;
-  const testLimit: number = 10;
-  async function testQuery(query: string) {
-    if (testCounter <= testLimit) {
-      await axios.get(encodeURI(query));
-      testCounter = testCounter + 1;
-      testQuery(query);
-    }
-  }
   switch (query) {
     case "__popular":
-      await testQuery(
-        encodeURI(`${process.env.MUSIC_SOURCE}/popular?only_eng=1&genreId=0`),
-      );
       const res: AxiosResponse = await axios.get(
         encodeURI(`${process.env.MUSIC_SOURCE}/popular?only_eng=1&genreId=0`),
       );
@@ -26,7 +14,6 @@ export default async function getData(query: IQuery): Promise<IResponse> {
       };
     default:
       let queryString: string = query.replaceAll(" ", "+");
-      await testQuery(encodeURI(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`));
       const { data }: AxiosResponse = await axios.get(
         encodeURI(`${process.env.MUSIC_SOURCE}/search?q=${queryString}`),
       );
